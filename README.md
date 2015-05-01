bef5b76a14c7888075efc4b315836e3bb3d424c3
Luke Davis <lukefable@gmail.com>
2015-05-01 07:54:39 +0000

    adds tests

---
+++ README.md
@@ -0,0 +1,3 @@
+#Lexalia
+
+an experimental template for generating new transpile-to-javascript languages, so anyone can make a new language like the cool kids.
---
+++ spec/lexaliaparser.js
@@ -0,0 +1,53 @@
+/* global lexaliaparser, describe, it, expect, should */
+
+describe('escodegen.generate()', function () {
+  'use strict';
+
+  it('exists', function () {
+    expect(escodegen.generate).to.be.a('function');
+
+  });
+
+  it('should parse an ast and return a string of Javascript', function () {
+    var ast = {
+      type: 'BinaryExpression',
+      operator: '+',
+      left: { type: 'Literal', value: 40 },
+      right: { type: 'Literal', value: 2 }
+    };
+    expect(escodegen.generate(ast)).to.equal('40 + 2');
+  });
+
+  it('does something else', function () {
+    expect(true).to.equal(false);
+  });
+
+  // Add more assertions here
+});
+
+describe('lexalia.parse()', function () {
+  'use strict';
+
+  var prog = `do(define(x, 10),
+     if(>(x, 5),
+        print("large"),
+        print("small")))`;
+
+  it('exists', function () {
+    expect(lexalia.parse).to.be.a('function');
+  });
+
+  it('returns an object when handed a program string', function () {
+
+    expect(lexalia.parse(prog)).to.be.an('object');
+  });
+
+  it('escodegen is present', function () {
+    expect(escodegen.generate).to.be.a('function');
+  });
+
+  it('returns a js string when passed the ast to escodegen',function(){
+    expect(escodegen.generate(lexalia.parse(prog))).to.be.a('string');
+  });
+  
+});
